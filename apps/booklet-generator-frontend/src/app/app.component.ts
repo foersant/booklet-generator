c95f1270cdeaf973c5210a6fc321351f8c1fc4f6
antkefoerster <gitafo@antke.de>
2022-11-23 18:30:22 +0100

    Initial commit

---
+++ apps/booklet-generator-frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'booklet-generator-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'booklet-generator-frontend';
+}
