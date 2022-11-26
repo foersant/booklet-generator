644d1cf361431127375efa2c0e6d7ccaed674463
antkefoerster <gitafo@antke.de>
2022-11-26 18:06:17 +0100

    Add Swagger

--- apps/booklet-generator-backend/src/main.ts
+++ apps/booklet-generator-backend/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -12,6 +13,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  const config = new DocumentBuilder()
+    .setTitle('IQB Testcenter - Booklet Generator')
+    .setDescription('API zum Erzeugen von Booklets für das IQB Testcenter')
+    .setVersion('1.0')
+    .addTag('Booklet Generator')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
